6d28051b7fbaf0adfb86153bb83259da013484fe
William Asprilla <william.asprilla.ceballos@gmail.com>
2022-12-04 15:06:42 -0500

    Envio de correo

---
+++ Omegan/Omegan.API/Controllers/EmailController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Omegan.Application.Features.Companies.Queries.GetCompanyByIdWithArchives;
+using Omegan.Application.Utils;
+
+namespace Omegan.API.Controllers
+{
+    
+    public class EmailController : ControllerBase
+    {
+
+        [HttpGet("SendEmail")]
+        public async Task<IActionResult> SendEmail()
+        {
+            SendEmail email = new SendEmail();
+            string to = "william.asprilla.ceballos@gmail.com";
+            string subject = "Notificacion Omegan";
+            string EmailBody = "Esta es una prueba del envio de correos desde Omegan";
+            string from = "wasprilla@hotmail.com";
+            string servidor = "smtp.office365.com";
+            string password = "";
+            int  port = 587;
+            await email.Send(to, subject, EmailBody, from, servidor, password, port);
+
+            return Ok("Correo enviado");
+        }
+    }
+}
---
+++ Omegan/Omegan.Application/Utils/SendEmail.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omegan.Application.Utils
+{
+    public class SendEmail
+    {
+        public async Task<string> Send(string to, string subject, string body, string from, string servidor, string password, int port)
+        {
+
+            try
+            {
+                // Se crea el mensaje
+                string[] direcciones = to.Split(',');
+
+                MailMessage mensaje = new MailMessage();
+                foreach (string email in direcciones)
+                {
+                    if (!string.IsNullOrEmpty(email))
+                    {
+                        mensaje.To.Add(email);
+                    }
+                }
+
+
+                mensaje.From = new MailAddress(from);
+                mensaje.Subject = subject;
+                mensaje.Body = body;
+                mensaje.IsBodyHtml = true;
+
+                // Se establece una referencia la servidor SMTP
+                SmtpClient cliente = new SmtpClient(servidor, port);
+
+                // Establece la configuraci?n del cliente
+                cliente.DeliveryMethod = SmtpDeliveryMethod.Network;
+                cliente.EnableSsl = true;
+                cliente.UseDefaultCredentials = false;
+                cliente.Credentials = new NetworkCredential(from, password);
+
+                {
+                    cliente.Send(mensaje);
+                }
+
+                return "Correo enviado";
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+           
+
+        }
+
+    }
+}
